033882f48798e34b458b50c944abc45a0b2aad8e
Vishwa Shah <vishwashah180701@gmail.com>
2021-10-01 22:20:37 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        vector<int> v{A,B,C};
+        sort(v.begin(),v.end());
+        return v[1];
+        
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
