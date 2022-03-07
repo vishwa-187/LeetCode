a9f74958424137790f94b8a6ce5bfad85fc30a4b
Vishwa Shah <vishwashah180701@gmail.com>
2022-03-07 11:50:53 +0530

    Time: 4 ms (56.24%), Space: 8 MB (23.85%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        int n = nums.size();
-        map <int , int> m;
-        
-        for(int i = 0;i<n;i++){
-            m[nums[i]]++;
-        }
+        map<int,int> m;
         int sum = 0;
-        for(auto i : m){
-            if(i.second==1){
+        for(int i = 0;i<nums.size();i++)
+            m[nums[i]]++;
+        for(auto i:m){
+            if(i.second == 1)
                 sum = sum + i.first;
-            }
         }
         return sum;
+            
     }
 };
